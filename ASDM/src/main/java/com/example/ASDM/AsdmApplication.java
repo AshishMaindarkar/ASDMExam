5f039e435b855a3a828cb1732c9b9bd95a199b36
mayur@gmai.com <shubham.dhalmare@gmail.com>
2019-06-15 08:22:03 +0530

    first commit

---
+++ ASDM/src/main/java/com/example/ASDM/AsdmApplication.java
@@ -0,0 +1,13 @@
+package com.example.ASDM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsdmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsdmApplication.class, args);
+	}
+
+}
